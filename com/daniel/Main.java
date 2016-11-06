f53b255b5668242a6d1464be17a634b248a8607b
DanielsLPecke <daniel.letsplay007@gmail.com>
2016-11-06 13:13:10 +0100

    fertig

--- com/daniel/Main.java
+++ com/daniel/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 101; i++){
-			System.out.println("Hallo, Welt!\ndu bist zu "+MathClass.getAdd(i,i)+"% toll");
+			System.out.println("Hallo, Welt!\ndu bist zu "+MathClass.add(i,i)+"% toll");
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
--- com/daniel/MathClass.java
+++ com/daniel/MathClass.java
@@ -6,7 +6,22 @@ public class MathClass {
 		return Math.PI;
 	}
 	
-	public static int getAdd(int i, int k){
+	public static int add(int i, int k){
+		return i + k;
+	}
+	public static int add(byte i, byte k){
+		return i + k;
+	}
+	public static int add(short i, short k){
+		return i + k;
+	}
+	public static float add(float i,float k){
+		return i + k;
+	}
+	public static long add(long i, long k){
+		return i + k;
+	}
+	public static double add(double i, double k){
 		return i + k;
 	}
 
