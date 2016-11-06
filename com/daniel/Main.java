65841607d3b7874c50492f2269047a8b51bc16d1
DanielsLPecke <daniel.letsplay007@gmail.com>
2016-11-06 09:31:34 +0100

    Main class is better now

--- com/daniel/Main.java
+++ com/daniel/Main.java
@@ -1,9 +1,16 @@
 package com.daniel;
-public class Main{
 
-public static void main(String[] args){
-	System.out.println("Hallo, Welt!");
-}
+public class Main {
 
+	public static void main(String[] args) {
+		for (byte i = 0; i < 101; i++){
+			System.out.println("Hallo, Welt!\ndu bist zu "+i+"% toll");
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
